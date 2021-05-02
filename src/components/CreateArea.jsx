4d110f34b9b80d5c69cc0e6198e790434fdaf405
matt <matthewdao73@gmail.com>
2021-05-02 13:01:01 +0100

    added more features

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
 
-function CreateArea() {
+function CreateArea(prop) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const [expand, setExpand] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    prop.onAdd(note);
+    event.preventDefault();
+    setNote({
+      title: "",
+      content:""
+    });
+    setExpand(false);
+  }
+
+  function expandNote(){
+    setExpand(true);
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          onChange={handleChange}
+          onClick={expandNote}
+          name="title"
+          placeholder="Title"
+          value={note.title}
+        />
+        {expand == true ? <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={note.content}
+          /> : <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="1"
+          value={note.content}
+          />
+        }
+        
+        <button onClick={submitNote}>
+          <AddIcon />
+        </button>
       </form>
     </div>
   );
